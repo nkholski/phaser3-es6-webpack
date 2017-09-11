3482238c0ca30a53ede7ee460bec0a45439cb8e7
Niklas Berg <niklas@niklasdator.lan>
2017-09-11 14:08:28 +0200

    Fixed deploy

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -2,10 +2,8 @@ var path = require('path')
 var webpack = require('webpack')
 
 // Phaser webpack config
-var phaserModule = path.join(__dirname, '/node_modules/phaser-ce/')
-var phaser = path.join(phaserModule, 'build/custom/phaser-split.js')
-var pixi = path.join(phaserModule, 'build/custom/pixi.js')
-var p2 = path.join(phaserModule, 'build/custom/p2.js')
+var phaserModule = path.join(__dirname, '/node_modules/phaser/')
+var phaser = path.join(phaserModule, 'src/phaser.js')
 
 var definePlugin = new webpack.DefinePlugin({
   __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'false'))
@@ -17,7 +15,7 @@ module.exports = {
       'babel-polyfill',
       path.resolve(__dirname, 'src/main.js')
     ],
-    vendor: ['pixi', 'p2', 'phaser', 'webfontloader']
+    vendor: ['phaser']
 
   },
   output: {
@@ -40,9 +38,7 @@ module.exports = {
   module: {
     rules: [
       { test: /\.js$/, use: ['babel-loader'], include: path.join(__dirname, 'src') },
-      { test: /pixi\.js/, use: ['expose-loader?PIXI'] },
-      { test: /phaser-split\.js$/, use: ['expose-loader?Phaser'] },
-      { test: /p2\.js/, use: ['expose-loader?p2'] }
+      { test: /phaser-split\.js$/, use: ['expose-loader?Phaser'] }
     ]
   },
   node: {
@@ -53,8 +49,6 @@ module.exports = {
   resolve: {
     alias: {
       'phaser': phaser,
-      'pixi': pixi,
-      'p2': p2
     }
   }
 }
