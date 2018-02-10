85918200e57639734730048036465505d119a140
Niklas Berg <niklas.b3rg@gmail.com>
2018-02-10 18:33:27 +0100

    Corrupted audiosprite replaced + few other things

--- rawAssets/sfx.old/higherVolume.sh
+++
@@ -1,9 +0,0 @@
-#
-# The volume of the wav-files i found was too low. This makes it 20 times higher :-)
-#
-
-for i in *.wav;
-  do name=`echo $i | cut -d'.' -f1`;
-  echo $name;
-  ffmpeg -i "$i" -filter:a "volume=20" "./higherVolume/${name}.wav"
-done
