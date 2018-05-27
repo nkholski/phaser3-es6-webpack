020dd75fcc08020333af7f4aa0d0311830d87730
Niklas Berg <niklas.b3rg@gmail.com>
2018-05-27 07:41:10 +0200

    Phaser 3.9.0 update

--- src/sprites/Turtle.js
+++ src/sprites/Turtle.js
@@ -8,7 +8,7 @@ export default class Turtle extends Enemy {
         this.sliding = false;
         this.type = 'turtle';
         this.body.setSize(12, 12);
-        this.body.offset.set(2, 12);
+        this.body.offset.set(2, 2);
     }
 
     update() {
