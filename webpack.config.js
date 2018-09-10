4f2a98fcd52802819f96d654afff5f47fdaf1106
unknown <niklas.b3rg@gmail.com>
2018-09-10 21:31:52 +0200

    Upgraded to Babel 7 and Phaser 3.12

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,6 @@ var definePlugin = new webpack.DefinePlugin({
 module.exports = {
     entry: {
         app: [
-            'babel-polyfill',
             path.resolve(__dirname, 'src/main.js')
         ],
         vendor: ['phaser']
