9006425e6de0992e5ac949bb768815cc3c06083f
Moritz Schoenauer <moritz.schoenauer@buhl-medien.de>
2019-05-27 16:38:51 +0200

    fix video sizing with a very bad hack

--- src/scenes/YouAreSafe.js
+++ src/scenes/YouAreSafe.js
@@ -3,6 +3,12 @@ class YouAreSafe extends Phaser.Scene {
         super({
             key: 'YouAreSafe'
         });
+
+        this.style = document.createElement('style');
+        this.style.innerHTML = '#youaresafe {' +
+            'transform: inherit !important; position:fixed !important; width: 100vw; left: 0;' +
+            '}';
+        document.body.appendChild(this.style);
     }
     preload() {
         
@@ -20,29 +26,29 @@ class YouAreSafe extends Phaser.Scene {
         }
         multiplier = Math.floor(multiplier);
         console.log(multiplier);
-            
         
         this.video = document.createElement('video');
         //let video = document.getElementsByTagName('canvas')[0];
         this.video.playsinline = false;
         this.video.src = 'assets/video/youaresafe.mp4';
+        this.video.id = 'youaresafe';
         //this.video.width = 420;
         //this.video.height = 240;
         this.video.autoplay = false;
         
-        
-        //this.video.style = 'width: 800px; height: 900px;'
+        //this.video.style = 'width: 800px; height: 900px;'xv
         //
         //el.src = 'assets/video/youaresafe.mp4';
         this.element = this.add.dom(0,0, this.video);
+        // this.element.parent.style.width='100%';
+        // this.element.parent.height='100%';
         //this.element.setOrigin(0,0);
         
-        this.element.parent.style.width = 640 + 'px';
-        this.element.parent.style.height = 352 + 'px';
-        this.element.parent.style.left = ((sw - this.element.parent.offsetHeight) / 2) + 'px';
-        this.element.parent.style.top = ((sh - this.element.parent.offsetWidth) / 2 ) + 'px';
+        // this.element.parent.style.width = 90 + 'vh';
+        // this.element.parent.style.height = 352 + 'px';
+        // this.element.parent.style.left = ((sw - this.element.parent.offsetHeight) / 2) + 'px';
+        // this.element.parent.style.top = ((sh - this.element.parent.offsetWidth) / 2 ) + 'px';
         
-        console.log(this.element.parent)
         this.video.addEventListener('ended', (event) => {
     
             this.element.setVisible(false);
@@ -52,8 +58,8 @@ class YouAreSafe extends Phaser.Scene {
             gameScene.resumeAfterVideo();
         });
         
-        this.video.play(true)
-        this.video.style.transform = 'none';
+        this.video.play(true);
+        // this.video.style.transform = '';
     }
 
     update(time, delta) {
